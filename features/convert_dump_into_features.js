79dc9b14b308daf06c9aecaaa0581e0ea24caf9a
Paulo de Freitas <freitasneto.paulo@gmail.com>
2016-06-19 16:39:22 -0300

    Create file to process all data dump

---
+++ features/convert_dump_into_features.js
@@ -0,0 +1,33 @@
+const extractMatchFeatures = require("./extract_match_features");
+const fs = require("fs");
+const zlib = require("zlib");
+const JSONStream = require("JSONStream");
+
+const outputFileName = "matches.csv";
+
+var JSONParser =  JSONStream.parse("*");
+
+function listToString(list) {
+    return list.join(", ")+"\n";
+}
+
+var nrParsedMatches = 0;
+
+JSONParser.on("data", function(match) {
+    var teams = extractMatchFeatures(match);
+    fs.appendFileSync(outputFileName, listToString(teams.RADIANT));
+    fs.appendFileSync(outputFileName, listToString(teams.DIRE));
+    nrParsedMatches++;
+    if (nrParsedMatches % 100 == 0) {
+        console.log(nrParsedMatches, " parsed matches.");
+    }
+});
+
+JSONParser.on("end", function() {
+    console.log("Finished.");
+    console.log("Print min max");
+});
+
+var dumpFileName = "/home/paulo/yaspdump/yasp-dump-2015-12-18.json.gz";
+var dumpStream = fs.createReadStream(dumpFileName);
+dumpStream.pipe(zlib.createGunzip()).pipe(JSONParser);
